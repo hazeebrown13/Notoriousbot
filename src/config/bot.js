95b18cdc9b4dbbaaf211078ea8bc07d95a547108
hazeebrown13 <hazeebrown13@gmail.com>
2026-07-08 07:57:25 +0800

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,9 +24,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "bot ni saintz karma",
         // Activity type number (0 = Playing).
-        type: 0,
+        type: 3,
       },
     ],
   },
@@ -91,13 +91,13 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699",
+      primary: "#000080",
       secondary: "#2F3136",
 
       // Standard status colors for success/error/warning/info messages.
-      success: "#57F287",
-      error: "#ED4245",
-      warning: "#FEE75C",
+      success: "#008000",
+      error: "#FF0000",
+      warning: "#FFFFC5",
       info: "#3498DB",
 
       // Neutral utility colors.
